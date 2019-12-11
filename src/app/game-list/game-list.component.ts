2fbfca7c93b61b025d87519c4e6defdd78cdd982
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2019-12-11 17:07:55 +0100

    TP1 - exo 3

--- src/app/game-list/game-list.component.ts
+++ src/app/game-list/game-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from './game';
 
 @Component({
   selector: 'app-game-list',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameListComponent implements OnInit {
 
+  entities: Game[] = [{
+    id: 1,
+    name: 'BattleBlock Theater',
+    description: 'Shipwrecked. Captured. Betrayed. Forced to perform for an audience of cats? '
+      + 'Yes, all that and more when you unlock BattleBlock Theater!'
+      + ' There\'s no turning back once you\'ve started on your quest to free over 300 of your imprisoned '
+      + 'friends from evil technological cats. ',
+    editor: 'The Behemoth',
+    image: 'https://steamcdn-a.akamaihd.net/steam/apps/238460/header.jpg?t=1561397233',
+    note: 9.8,
+    category: 'Jeu de platform multijoueur'
+  }, {
+    id: 2,
+    name: 'BattleBlock Theater 2',
+    description: 'Shipwrecked. Captured. Betrayed. Forced to perform for an audience of cats? '
+      + 'Yes, all that and more when you unlock BattleBlock Theater!'
+      + ' There\'s no turning back once you\'ve started on your quest to free over 300 of your imprisoned '
+      + 'friends from evil technological cats. ',
+    editor: 'The Behemoth',
+    image: 'https://steamcdn-a.akamaihd.net/steam/apps/238460/header.jpg?t=1561397233',
+    note: 9.8,
+    category: 'Jeu de platform multijoueur'
+  }];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  truncate(value: string) {
+    const words = value.split(' ', 20);
+
+    return words.join(' ') + (words.length > 20 ? + '...' : '');
   }
 
 }
