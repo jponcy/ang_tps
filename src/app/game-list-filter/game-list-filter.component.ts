9c6422d3dab26665ff809ac1770a7fe81768ea75
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2019-12-11 14:40:39 +0100

    TP1 - exo 2

--- src/app/game-list-filter/game-list-filter.component.ts
+++ src/app/game-list-filter/game-list-filter.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameListFilterComponent implements OnInit {
 
+  gameCategories = [ 'RTS', 'RPG', 'FPS' ];
+
   constructor() { }
 
   ngOnInit() {
